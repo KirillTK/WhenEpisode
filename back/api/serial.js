0386879ed858b53a5d68df4a1aea5a14cc9b3156
KirillTK <kirill99.tkachev@yandex.by>
2019-03-23 21:57:17 +0300

    #6/refactor: get all info about episodes

--- back/api/serial.js
+++ back/api/serial.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const request = require('request');
+const rp = require('request-promise');
+
+const rpForJson = rp.defaults({ json: true });
 const { apiKey } = require('../configurations/serialConfig');
 
 const router = express.Router();
@@ -9,7 +12,7 @@ router.get('/search/:search', (req, res, next) => {
 
   request(`https://api.themoviedb.org/3/search/tv?api_key=${apiKey}&language=en-US&query=${search}`, (error, response, body) => {
     if (error) next();
-    res.send(body);
+    res.send(JSON.parse(body));
   });
 });
 
@@ -18,17 +21,22 @@ router.get('/getTvInfo/:id', (req, res, next) => {
 
   request(`https://api.themoviedb.org/3/tv/${id}?api_key=${apiKey}&language=en-US`, (error, response, body) => {
     if (error) next();
-    res.send(body);
+    res.send(JSON.parse(body));
   });
 });
 
-router.get('/getTvEpisodes/:id', (req, res, next) => {
+router.get('/getTvEpisodes/:id', async (req, res, next) => {
   const { id } = req.params;
+  const serialResponse = await rp(`https://api.themoviedb.org/3/tv/${id}?api_key=${apiKey}&language=en-US`);
+  const serial = JSON.parse(serialResponse);
 
-  request(`https://api.themoviedb.org/3/tv/${id}/season/1?api_key=${apiKey}&language=en-US`, (error, response, body) => {
-    if (error) next();
-    res.send(body);
+  const promises = [];
+  serial.seasons.forEach((season) => {
+    promises.push(rpForJson(`https://api.themoviedb.org/3/tv/${id}/season/${season.season_number}?api_key=${apiKey}&language=en-US`));
   });
+
+  const result = await Promise.all(promises);
+  res.send(result);
 });
 
 module.exports = router;
