dd1fa170f03f0063c6863805ffc0211076e26e40
KirillTK <kirill99.tkachev@yandex.by>
2019-03-23 18:21:43 +0300

    #1/refactor: export module correctly

--- back/server.js
+++ back/server.js
@@ -4,20 +4,20 @@ const mongoose = require('mongoose');
 const http = require('http');
 const passport = require('passport');
 const cookieParser = require('cookie-parser');
-const mongooseConnectionString = require('./configurations/mongooseConfig');
+const mongooseConfig = require('./configurations/mongooseConfig');
 
 const app = express();
 const server = http.createServer(app);
 
 mongoose.Promise = Promise;
 mongoose
-  .connect(mongooseConnectionString, { useNewUrlParser: true }).then(() => console.log('Mongoose up!'));
+  .connect(mongooseConfig.connectionString, { useNewUrlParser: true }).then(() => console.log('Mongoose up!'));
 
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(passport.initialize());
 app.use(passport.session());
-require('./api/user')(app, passport);
+// require('./api/user')(app, passport);
 
 
 server.listen(3000, () => console.log('listen 3000 port'));
